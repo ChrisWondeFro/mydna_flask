8a4099e7742202c59515a5a08f529ea8e573c4f3
Chrismanati <christianwondirad1213@gmail.com>
2023-07-10 09:49:05 -0600

    commit.

--- app/services/pdf_generator.py
+++ app/services/pdf_generator.py
@@ -1,8 +1,12 @@
 from reportlab.pdfgen import canvas
+from datetime import datetime
 
 class PDFGenerator:
     def write_report(self, summary_html):
-        c = canvas.Canvas("DNA_Health_Assessment_Report.pdf")
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f'DNA_Health_Assessment_Report_{timestamp}.pdf'
+
+        c = canvas.Canvas(filename)
         textobject = c.beginText()
         textobject.setTextOrigin(10, 730)
         lines = summary_html.split('\n')
@@ -10,3 +14,5 @@ class PDFGenerator:
             textobject.textLine(line.strip())
         c.drawText(textobject)
         c.save()
+
+        return filename
