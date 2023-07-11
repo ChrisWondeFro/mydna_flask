1c13c6d25bd0cbf11ca61f78a7ffcf52929f9510
Chrismanati <christianwondirad1213@gmail.com>
2023-07-11 04:25:27 -0600

    commit

--- app/services/summary_generator.py
+++ app/services/summary_generator.py
@@ -4,6 +4,18 @@ from flask import render_template, current_app
 
 env = Environment(loader=FileSystemLoader('app/templates'))
 
+
+def generate_summary_data(self, organized_data):
+    summary_data = []
+    total_variants = sum(len(v) for cs in organized_data.values() for t in cs.values() for v in t)
+    summary_data.append(f"A total of {total_variants} variants were analyzed.")
+
+    for cs, types in organized_data.items():
+        cs_count = sum(len(v) for t in types.values() for v in t)
+        summary_data.append(f"{cs_count} variants were found to be {cs}.")
+
+    return summary_data
+
 class SummaryGenerator:
     @staticmethod
     def generate_summary(organized_data, clinical_significance_counts, image_url):
